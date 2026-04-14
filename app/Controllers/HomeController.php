602d42c16248f59de1b7c39bb212f014016561a3
codewithelie <me@elyh.fr>
2026-04-14 16:05:36 +0200

    Exercice about page

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,7 +7,6 @@ class HomeController extends Controller {
     }
 
     public function about() {
-        echo "<h1>À propos</h1>";
-        echo "<p>Voici les informations sur l'entreprise.</p>";
+        $this->view('home/about');
     }
 }
